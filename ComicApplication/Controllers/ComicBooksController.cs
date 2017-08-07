f315ebe9fc088c63dad8b82fb5539d1814c009c6
Abolade <ABOLADLX@legal.regn.net>
2017-08-07 15:34:27 +0100

    initial commit with first detail page

---
+++ ComicApplication/Controllers/ComicBooksController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ComicApplication.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
