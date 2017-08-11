4d097abade294294e1b03f51069b940148eb7c9e
Abolade <ABOLADLX@legal.regn.net>
2017-08-11 12:07:53 +0100

    adding index method in controller with view to display list of comic books

--- ComicApplication/Controllers/ComicBooksController.cs
+++ ComicApplication/Controllers/ComicBooksController.cs
@@ -15,6 +15,12 @@ namespace ComicApplication.Controllers
         {
             _comicBookRepository = new ComicBookRepository();
         }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+            return View(comicBooks);
+        }
         public ActionResult Detail(int? id)
         {
             if (id == null)
